c735e17daee01db9bf42624ad1873b828cb1ec45
Francisco Paredes <frannkx@gmail.com>
2023-06-21 16:27:41 -0500

    se agrega carpeta Hashicorp Vault

---
+++ Hashicorp/Vault/vault-reference.sh
@@ -0,0 +1,11 @@
+
+# Ver keys y Root token
+vault operator init
+
+#Reference: 
+# K8s https://developer.hashicorp.com/vault/docs/platform/k8s
+# Vault Helm chart https://github.com/hashicorp/vault-helm
+# Integrate K8s vault https://developer.hashicorp.com/vault/tutorials/kubernetes/kubernetes-external-vault
+# CSI on AKS https://learn.microsoft.com/en-us/azure/aks/csi-storage-drivers
+# Vault CSI provider https://developer.hashicorp.com/vault/tutorials
+# Kubernetes https://developer.hashicorp.com/vault/docs/secrets/kubernetes
