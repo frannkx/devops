004cccceb75103a6cc945546865837571a87cf37
Francisco Paredes <frannkx@gmail.com>
2023-12-08 00:21:27 -0500

    Creacion de ramas con GIT

--- Virtualization/Linux/GitHub Challenge/Modulos.sh
+++ Virtualization/Linux/GitHub Challenge/Modulos.sh
@@ -186,5 +186,221 @@ git revert --no-edit HEAD
 git log -n1
 
 
-## Colaboración con Git 
+## Modulo 3 - Colaboración con Git 
+
+# Clonar repositorio 
+  git clone
+
+   #SSH (por ejemplo, git@example.com:alice/Cats
+
+# Actualizar cambios desde el origen
+git pull
+
+#Creación de solicitudes de incorporación de cambios (git request-pull)
+
+git request-pull -p origin/main .
+
+##Creación de un remoto (git remote) y finalización de la solicitud de incorporación de cambios (git pull)
+
+#configurar el repositorio de otro desarrollador como remoto
+git remote
+
+#use ese remoto para las incorporaciones y las solicitudes de incorporación de cambios mediante el comando
+git pull
+
+#En segundo plano, git pull es una combinación de dos operaciones más sencillas
+git fetch #, que obtiene los cambios
+git merge #, que combina esos cambios en el repositorio.
+
+#Ejecutar incorporacion de cambios
+git pull remote-alice main
+
+#Colaboración mediante un repositorio compartido, repositorio vacio
+
+mkdir Shared.git
+cd Shared.git
+git init --bare #Ahora use el siguiente comando para crear un repositorio vacío en el directorio compartido:
+
+git symbolic-ref HEAD refs/heads/main # cambiar la rama HEAD para que apunte a otra rama; en este caso es la rama main
+
+# obtener el contenido de su repositorio en el repositorio compartido.
+cd ../Cats
+git remote add origin ../Shared.git
+git push origin main
+
+#indicar a Git la rama cuyo seguimiento se va a efectuar.
+git branch --set-upstream-to origin/main
+
+#Configuración para colaboradores
+cd ..
+mkdir Bob
+cd Bob
+
+#Ahora, clone el repositorio compartido (asegúrese de incluir el punto al final del comando):
+
+#BOB
+git clone ../Shared.git .
+
+#Alice
+cd ../Alice
+git remote set-url origin ../Shared.git
+
+#Inicio de la colaboración
+
+git diff origin -- index.html #ver diferencias de cambios conjuntos en mimsmos archivos
+
+git stash #guardar provisionalmente los cambios de Alice:
+
+#incorporar los cambios con seguridad, después de lo cual puede "sacar" los cambios guardados provisionalmente,
+git pull
+git stash pop
+
+## Modulo 4 - Edición de código mediante creación de ramas y combinación en Git
+
+#Crear rama
+git branch
+
+#cambiar entre ramas 
+git checkout
+
+#Combinar con rama principal
+git merge
+
+
+#Ejercicio: Creación de un repositorio vacío compartido
+mkdir Shared.git
+cd Shared.git
+
+#Ahora, ejecute el comando siguiente para crear un repositorio vacío en el directorio compartido:
+git init --bare
+
+#Establezca el nombre de la rama predeterminada del nuevo repositorio. Para realizar este paso, puede cambiar la rama HEAD para que apunte a otra rama; en este caso, la rama main:
+git symbolic-ref HEAD refs/heads/main
+
+#Clonación del repositorio compartido para Bob#
+cd ..
+mkdir Bob
+
+cd Bob
+git clone ../Shared.git .
+git config user.name Bob
+git config user.email bob@contoso.com
+git symbolic-ref HEAD refs/heads/main
+
+#Incorporación de archivos base
+
+touch index.html
+mkdir Assets
+touch Assets/site.css
+git add .
+git commit -m "Create empty index.html and site.css files"
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset='UTF-8'>
+    <title>Our Feline Friends</title>
+    <link rel="stylesheet" href="CSS/site.css">
+  </head>
+  <body>
+    <nav><a href="./index.html">home</a></nav>
+    <h1>Our Feline Friends</h1>
+    <p>Eventually we will put cat pictures here.</p>
+    <footer><hr>Copyright (c) 2021 Contoso Cats</footer>
+  </body>
+</html>
+
+cd Assets
+code site.css
+
+h1, h2, h3, h4, h5, h6 { font-family: sans-serif; }
+body { font-family: serif; background-color: #F0FFF8; }
+nav, footer { background-color: #C0D8DF; }
+
+cd ..
+git add .
+git commit -m "Add simple HTML and stylesheet"
+git push --set-upstream origin main
+
+#Mensaje
+warning: push.default is unset; its implicit value has changed in
+Git 2.0 from 'matching' to 'simple'. To squelch this message
+and maintain the traditional behavior, use:
+
+  git config --global push.default matching
+
+To squelch this message and adopt the new behavior now, run:
+
+  git config --global push.default simple
+
+When push.default is set to 'matching', git will push local branches
+to the remote branches that already exist with the same name.
+
+Since Git 2.0, Git defaults to the more conservative 'simple'
+behavior, which only pushes the current branch to the corresponding
+remote branch that 'git pull' uses to update the current branch.
+
+See 'git help config' and search for 'push.default' for further information.
+(the 'simple' mode was introduced in Git 1.7.11. Use the similar mode
+'current' instead of 'simple' if you sometimes use older versions of Git)
+#
+
+git config --global push.default simple
+
+cd ..
+mkdir Alice
+
+cd Alice
+git clone ../Shared.git .
+git config user.name Alice
+git config user.email alice@contoso.com
+
+ls
+git status
+
+git branch add-style
+git checkout add-style
+
+code site.css
+
+.cat { max-width: 40%; padding: 5 }
+
+git commit -a -m "Add style for cat pictures"
+
+git checkout main
+git pull
+
+git merge --ff-only add-style
+git push
+
+#Creación de una rama para Bob
+
+cd ../Bob
+git checkout -b add-cat
+
+wget https://github.com/MicrosoftDocs/mslearn-branch-merge-git/raw/main/git-resources.zip
+unzip git-resources.zip
+
+mv bobcat2-317x240.jpg Assets/bobcat2-317x240.jpg
+rm git-resources.zip
+rm bombay-cat-180x240.jpg
+
+code index.html
+
+<img src="Assets/bobcat2-317x240.jpg" />
+
+git status
+
+git add .
+git commit -a -m "Add picture of Bob's cat"
+
+git checkout main
+git pull
+
+git merge add-cat --no-edit
+git push
+
+#Sincronización de los repositorios
+cd ../Alice
+git pull
 
